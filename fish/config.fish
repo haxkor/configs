765ea68d623f8ed810a06e03ec26db470f1b7b16
Jasper Ruehl <jasper.ruehl@tum.de>
2026-08-26 09:44:25 +0200

    rm old files

--- fish/config.fish
+++
@@ -1,7 +0,0 @@
-if status is-interactive
-    set fish_greeting
-    abbr --add vim nvim
-
-    abbr --add py3 python3
-    alias vim nvim
-end
--- nvim/init.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.expandtab = true    -- Tabs zu Spaces konvertieren
-vim.opt.tabstop = 4         -- Wie breit ein Tab-Zeichen angezeigt wird
-vim.opt.softtabstop = 4     -- Wie viele Spaces beim Drücken von Tab/Backspace eingefügt/gelöscht werden
-vim.opt.shiftwidth = 4      -- Einrückungstiefe bei >>, << und Auto-Indent
-vim.opt.smartindent = true  -- Automatisches Einrücken (C-like)
-vim.opt.autoindent = true   -- Übernimmt Einrückung der vorherigen Zeile
-
-
-require("config.lazy")
